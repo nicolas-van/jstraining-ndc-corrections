2b261e8cc7d9b9dcc2c00aeaec93a6d65c589a0d
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-11-10 16:11:19 +0100

    Added router

--- app.js
+++ app.js
@@ -2,7 +2,21 @@
 (function () {
     "use strict";
 
-    var mySuperApp = angular.module('mySuperApp', []);
+    var mySuperApp = angular.module('mySuperApp', ['ngRoute']);
+
+    mySuperApp.config(['$routeProvider', function($routeProvider) {
+        $routeProvider.
+          when('/page1', {
+            templateUrl: 'views/page1.html',
+            controller: 'MainPageCtrl',
+          }).
+          when('/page2', {
+            templateUrl: 'views/page2.html',
+          }).
+          otherwise({
+            redirectTo: '/page1',
+          });
+    }]);
 
     mySuperApp.controller("MainPageCtrl", ["$scope", function($scope) {
 
