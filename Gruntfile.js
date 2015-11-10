22755b26e3bfcd30267f7504035ec17050217d73
Nicolas Vanhoren <nicolas.vanhoren@gmail.com>
2015-11-10 17:44:02 +0100

    First async exercise

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,13 +3,31 @@ module.exports = function(grunt) {
 
   grunt.initConfig({
     connect: {
-      server: {
-        options: {
-            base: ['.'],
-            keepalive: true,
-        },
-      },
-    },
+        server: {
+            options: {
+                base: ['.'],
+                keepalive: true,
+                middleware: function(connect, options, middlewares) {
+                    middlewares.unshift(function myMiddleware(req, res, next) {
+                        var url = require('url').parse(req.url, true);
+                        if (url.pathname === "/add") {
+                            res.end("" + (Number(url.query.arg1) + Number(url.query.arg2)));
+                        } else if (url.pathname === "/sub") {
+                            res.end("" + (Number(url.query.arg1) - Number(url.query.arg2)));
+                        } else if (url.pathname === "/mult") {
+                            res.end("" + (Number(url.query.arg1) * Number(url.query.arg2)));
+                        } else if (url.pathname === "/div") {
+                            res.end("" + (Number(url.query.arg1) / Number(url.query.arg2)));
+                        } else {
+                            return next();
+                        }
+                    });
+
+                    return middlewares;
+                },
+            }
+        }
+    }
   });
 
   grunt.loadNpmTasks('grunt-contrib-connect');
--- app.js
+++ app.js
@@ -5,13 +5,16 @@
     var mySuperApp = angular.module('mySuperApp', ['ngRoute']);
 
     mySuperApp.config(['$routeProvider', function($routeProvider) {
-        $routeProvider.
-          when('/page1', {
+        $routeProvider.when('/page1', {
             templateUrl: 'views/page1.html',
             controller: 'MainPageCtrl',
-          }).
-          when('/page2', {
+          });
+        $routeProvider.when('/page2', {
             templateUrl: 'views/page2.html',
+          }).
+          when('/async', {
+            templateUrl: 'views/async.html',
+            controller: 'AsyncCtrl',
           }).
           otherwise({
             redirectTo: '/page1',
@@ -26,6 +29,27 @@
         $scope.copyText = function() {
             $scope.displayedTexts.push({txt: $scope.inputText});
         };
+
+    }]);
+
+    mySuperApp.controller("AsyncCtrl", ["$scope", "$http", function($scope, $http) {
+
+        $scope.results = [];
+
+        var p1 = $http.get("/add?arg1=1&arg2=2").then(function(result) {
+            $scope.results.push(result.data);
+            return $http.get("/add?arg1=" + result.data + "&arg2=3");
+        }).then(function(result) {
+            $scope.results.push(result.data);
+            return $http.get("/add?arg1=" + result.data + "&arg2=4");
+        }).then(function(result) {
+            $scope.results.push(result.data);
+            return $http.get("/add?arg1=" + result.data + "&arg2=5");
+        }).then(function(result) {
+            $scope.results.push(result.data);
+        });
+
+        console.log("b");
 
     }]);
 
